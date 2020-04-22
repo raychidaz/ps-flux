f874082540afb8062c1e645be80815bbb591c139
Ray Chidaz <raychidaz@gmail.com>
2020-04-22 11:26:05 +0100

    refactor CoursesPage from class component to function component, manage state with useState and useEffect hooks, refactor monolithic CoursePage component to pass data down to Courselist component via props (container/presentation) , use Proptypes to document and validate props

--- src/components/CoursesPage.jsx
+++ src/components/CoursesPage.jsx
@@ -1,43 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import CourseList from "./CourseList";
 
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: [],
-  };
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
 
-  componentDidMount() {
-    getCourses().then((courses) => this.setState({ courses: courses }));
-  }
+  useEffect(() => {
+    getCourses().then((_courses) => setCourses(_courses));
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map((course) => {
-              return (
-                <tr key={course.id}>
-                  <td>{course.title}</td>
-                  <td>{course.tauthorID}</td>
-                  <td>{course.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Courses</h2>
+      <CourseList courses={courses} />
+    </>
+  );
 }
 
 export default CoursesPage;
